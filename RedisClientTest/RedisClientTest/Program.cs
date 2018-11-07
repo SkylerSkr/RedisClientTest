02c2f236ec95ce82737f3760ef722fa942074cf8
TangoKe <41276818+TangoKe@users.noreply.github.com>
2018-11-07 09:29:20 +0800

    feature/first

---
+++ RedisClientTest/RedisClientTest/Program.cs
@@ -0,0 +1,59 @@
+﻿using ServiceStack.Redis;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建比较器
+            var comparator= TaskComparator.GetInstance(1000);
+
+            //ServiceStack.Redis
+            RedisClient redisClient = new RedisClient("192.168.8.204", 6379, "superrd", 0);
+            //StackExchange.Redis
+            IConnectionMultiplexer proxy = ConnectionMultiplexer.Connect("192.168.8.204:6379,password=superrd,DefaultDatabase=0");
+            IDatabase db = proxy.GetDatabase();
+
+
+            //string千次读写对比
+            Console.WriteLine("string千次读写对比");
+            comparator.Comparator(() => { redisClient.Set("S1", "AAA"); }, () => { db.StringSet("A2", "AAA"); });
+            comparator.Comparator(()=> { redisClient.Set("S1", "AAA",new TimeSpan(1,0,0));  },()=> { db.StringSet("A2", "AAA", new TimeSpan(1, 0, 0)); });
+            comparator.Comparator(() => { redisClient.Get<string>("A1"); }, () => { db.StringGet("A2"); });
+
+            //hash千次读写对比
+            Console.WriteLine("hash千次读写对比");
+            comparator.Comparator(()=> { redisClient.SetEntryInHash("H1","id","xxx"); },()=> { db.HashSet("H2", "id", "xxx"); });
+            comparator.Comparator(()=> { redisClient.GetAllEntriesFromHash("H1"); },()=> { db.HashGet("H2", "id"); });
+
+            //list千次读写对比
+            Console.WriteLine("list千次读写对比");
+            comparator.Comparator(() => { redisClient.PushItemToList("L1","1"); }, () => { db.ListLeftPush("L2","1"); });
+            comparator.Comparator(()=> { redisClient.PopItemFromList("L1"); },()=>db.ListLeftPop("L1"));
+
+            //set千次读写对比
+            Console.WriteLine("set千次读写对比");
+            comparator.Comparator(()=> { redisClient.AddItemToSet("Set1", "xxx"); },()=> { db.SetAdd("Set2", "xxx"); });
+            comparator.Comparator(() => { redisClient.GetSetCount("Set1"); }, () => { db.SetLength("Set2"); });
+
+            //SortedSet千次读写对比
+            Console.WriteLine("SortedSet千次读写对比");
+            comparator.Comparator(()=> { redisClient.AddItemToSortedSet("SS1", "item1", 33); },()=> { db.SortedSetAdd("SS2", "item1", 33); });
+            comparator.Comparator(()=> { redisClient.GetSortedSetCount("SS1"); },()=> { db.SortedSetLength("SS2"); });
+
+            //Geo千次读写对比
+            Console.WriteLine("Geo千次读写对比");
+            comparator.Comparator(()=> { redisClient.GeoAdd("city", 33.33, 33.33, "shanghai"); },()=> { db.GeoAdd("city", 33.34, 33.34, "beijing"); });
+            comparator.Comparator(()=> { redisClient.FindGeoResultsInRadius("city","beijing", 333, "m"); },()=> { db.GeoRadius("city","beijing",333,GeoUnit.Meters); });
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RedisClientTest/RedisClientTest/TaskComparator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisClientTest
+{
+    public class TaskComparator
+    {
+        /// <summary>
+        /// 同步锁
+        /// </summary>
+        private static readonly object obj = new object();
+        public static TaskComparator comparator;
+        private int count;
+
+        private TaskComparator(int count) {
+            this.Count = count;
+        }
+
+        public int Count { get => count; set => count = value; }
+
+        public static TaskComparator GetInstance(int count=1)
+        {
+            if (comparator == null)
+            {
+                lock (obj)
+                {
+                    comparator = new TaskComparator(count);
+                }
+            }
+            return comparator;
+        }
+
+        public void Comparator(Action action1,Action action2)
+        {
+            if (comparator != null)
+            {
+                var time1 = TaskTimeConsuming(action1).TotalSeconds;
+                var time2 = TaskTimeConsuming(action2).TotalSeconds;
+                Console.WriteLine("前者耗时: {0} 秒，后者耗时: {1} 秒，前者用时少于后者: {2}", time1, time2, ((time2 - time1) / time1) * 100 + "%");
+            }
+        }
+
+        private TimeSpan TaskTimeConsuming(Action action)
+        {
+            var time0 = DateTime.Now;
+            for (int i = 0; i < Count; i++)
+            {
+                action.Invoke();
+            }
+            var time1 = DateTime.Now;
+            return time1 - time0;
+        }
+    }
+}
